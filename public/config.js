f2140687b659004f48ce08ee1bd4bac26dfa74b3
AndyWen <wenlei660185@gmail.com>
2025-02-07 09:49:16 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -18,12 +18,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '<strong>Blog</strong>',
+      text: 'Blog',
       url: 'https://wenlei.top/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/Andy17269'
     },
   ],
 };
