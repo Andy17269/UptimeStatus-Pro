9809b35aca8cb631d34ab53032e52b6724cd604d
AndyWen <wenlei660185@gmail.com>
2025-02-07 10:10:47 +0800

    Update uptimerobot.js

--- src/components/uptimerobot.js
+++ src/components/uptimerobot.js
@@ -37,7 +37,7 @@ function UptimeRobot({ apikey }) {
           }
           else if (data.uptime <= 0 && data.down.times === 0) {
             status = 'none';
-            text += '获取数据失败';
+            text += '获取数据失败,未知可用性';
           }
           else {
             status = 'down';
