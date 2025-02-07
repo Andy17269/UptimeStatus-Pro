cdea564306abed2bffdd19432cc904b3b0fe2611
AndyWen <wenlei660185@gmail.com>
2025-02-07 16:02:33 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -11,12 +11,12 @@ function Header() {
     <div id='header'>
       <div className='container'>
         <h1 className='logo'>{window.Config.SiteName}</h1>
-        <p>所有服务均正常运行</p>
         <div className='navi'>
           {window.Config.Navi.map((item, index) => (
             <strong> <Link key={index} to={item.url} text={item.text} /> </strong>
           ))}
         </div>
+        <p>当前无事件</p>
       </div>
     </div>
   );
